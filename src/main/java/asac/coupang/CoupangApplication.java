293bf5d898725d45e5e056d04ade91aeff3a9d5b
softwarerbfl <32202018@dankook.ac.kr>
2023-11-02 17:23:05 +0900

    Initial commit

---
+++ src/main/java/asac/coupang/CoupangApplication.java
@@ -0,0 +1,13 @@
+package asac.coupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangApplication.class, args);
+	}
+
+}
