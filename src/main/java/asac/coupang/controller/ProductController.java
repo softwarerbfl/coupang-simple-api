438ecf8c86fb68cd5b2651f6758d33173e24dbbd
softwarerbfl <32202018@dankook.ac.kr>
2023-11-06 18:07:47 +0900

    Seller 추가 API Product 추가 API Product detail 조회 API 구현

---
+++ src/main/java/asac/coupang/controller/ProductController.java
@@ -0,0 +1,30 @@
+package asac.coupang.controller;
+
+import asac.coupang.dto.ProductDetailDto;
+import asac.coupang.dto.ProductDto;
+import asac.coupang.entity.Product;
+import asac.coupang.repository.ProductRepository;
+import asac.coupang.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    // 상품 ID에 해당하는 상품의 디테일 정보 조회
+    @GetMapping("/product/{id}/detail")
+    public ResponseEntity<ProductDetailDto> getProductDetail(@PathVariable String id) {
+        return productService.getProductDetail(id);
+    }
+
+    // 상품 등록
+    @PostMapping("/product/add/{sellerId}")
+    public ResponseEntity<String> addProduct(@PathVariable String sellerId,
+                                              @RequestBody ProductDto dto){
+        return productService.addProduct(sellerId, dto);
+    }
+}
---
+++ src/main/java/asac/coupang/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package asac.coupang.repository;
+
+import asac.coupang.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
